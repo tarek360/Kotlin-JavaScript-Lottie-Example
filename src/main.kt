f35f04e197b3f8fa3ca5727dc5bd9c3200efcfa6
Ahmed Tarek <a.tarek360@gmail.com>
2018-11-11 18:39:22 +0700

    more static types

--- src/main.kt
+++ src/main.kt
@@ -1,7 +1,6 @@
 import org.w3c.dom.HTMLDivElement
+import org.w3c.dom.HTMLElement
 import kotlin.browser.document
-import kotlin.js.Json
-import kotlin.js.json
 
 fun main(args: Array<String>) {
 
@@ -9,19 +8,19 @@ fun main(args: Array<String>) {
 
     val jsonFileUrl = "https://www.lottiefiles.com/storage/datafiles/zc3XRzudyWE36ZBJr7PIkkqq0PFIrIBgp4ojqShI/newAnimation.json"
 
-    val animData = json(
-            Pair("container", div),
-            Pair("renderer", "svg"),
-            Pair("loop", true),
-            Pair("autoplay", true),
-            Pair("path", jsonFileUrl)
-    )
+    val animData = AnimData(div, "svg", true, true, jsonFileUrl)
 
     lottie.loadAnimation(animData)
 }
 
+data class AnimData(val container: HTMLElement,
+                    val renderer: String,
+                    val loop: Boolean,
+                    val autoplay: Boolean,
+                    val path: String)
+
 external val lottie: Lottie
 
 abstract external class Lottie {
-    fun loadAnimation(animData: Json)
+    fun loadAnimation(animData: AnimData)
 }
